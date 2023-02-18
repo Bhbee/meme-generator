98a265d96c8ceace1f8d677ef7a7b69ffe272f71
Bhbee <samuelchristiana38@gmail.com>
2023-02-19 00:32:45 +0100

    updated meme generator

--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import './meme.css'
+import memesData from "./memesData.js"
 
-const Meme = () => {
-  return (
-    <main>
-        <form className='form'>
-            <input type='text' placeholder='Top text' className='form-inpuut' />
-            <input type='text' placeholder='Bottom text' className='form-inpuut' />
-            <button className='form-button'>Get a new meme 🖼</button>
-        </form>
-    </main>
+export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+    return (
+        <main>
+            <div className='form'>
+                <input type='text' placeholder='Top text' className='form-inpuut' />
+                <input type='text' placeholder='Bottom text' className='form-inpuut' />
+                <button className='form-button' onClick={getMemeImage}>Get a new meme 🖼</button>
+            </div>
+            <img src={memeImage} className="meme-img" />
+        </main>
   )
 }
-
-export default Meme
